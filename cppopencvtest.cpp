12bee3c32a833a0cb06fd9d3822532a6eb82f171
happy <happy@WebgenUbuntu.myguest.virtualbox.org>
2023-11-04 09:30:27 +0000

    cpp test

--- cppopencvtest.cpp
+++ cppopencvtest.cpp
@@ -1,6 +1,6 @@
-#include <opencv2/opencv.hpp>
-#include <opencv2/cudaimgproc.hpp>
-#include <opencv2/cudaarithm.hpp>
+#include </opencv2/opencv.hpp>
+#include </opencv2/cudaimgproc.hpp>
+#include </opencv2/cudaarithm.hpp>
 
 int main() {
     // Load two images
