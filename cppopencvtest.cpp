5b1218597b5fc181a36238b51321c39674a4152e
happy <happy@WebgenUbuntu.myguest.virtualbox.org>
2023-11-04 09:32:27 +0000

    cpp test

--- cppopencvtest.cpp
+++ cppopencvtest.cpp
@@ -1,5 +1,5 @@
 #include </usr/include/opencv4/opencv2/opencv.hpp>
-#include <//usr/include/opencv4/opencv2/cudaimgproc.hpp>
+#include </usr/include/opencv4/opencv2/cudaimgproc.hpp>
 #include </usr/include/opencv4/opencv2/cudaarithm.hpp>
 
 int main() {
